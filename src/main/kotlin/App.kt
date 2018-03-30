df096ca53d29c97d5f1ff72510a35d8071963e4b
Manuel Huber <dev@manuel-huber.de>
2018-03-30 12:16:24 +0200

    code style

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -3,27 +3,30 @@ import java.awt.GridBagConstraints
 import java.awt.GridBagLayout
 import javax.swing.*
 
-
 class App : JPanel(GridBagLayout()) {
     private var textField: JTextField = JTextField(20)
     private var textArea: JTextArea = JTextArea(5, 20)
     private var autoCorrect: Autocomplete = Autocomplete()
 
     init {
+        initGui()
+    }
+
+    private fun initGui() {
         textArea.isEditable = false
-        val scrollPane = JScrollPane(textArea)
 
-        //Add Components to this panel.
-        val c = GridBagConstraints()
-        c.gridwidth = GridBagConstraints.REMAINDER
+        val constraints = GridBagConstraints()
+        constraints.gridwidth = GridBagConstraints.REMAINDER
 
-        c.fill = GridBagConstraints.HORIZONTAL
-        add(textField, c)
+        constraints.fill = GridBagConstraints.HORIZONTAL
+        add(textField, constraints)
 
-        c.fill = GridBagConstraints.BOTH
-        c.weightx = 1.0
-        c.weighty = 1.0
-        add(scrollPane, c)
+        constraints.fill = GridBagConstraints.BOTH
+        constraints.weightx = 1.0
+        constraints.weighty = 1.0
+
+        add(JScrollPane(textArea), constraints)
+
         SwingObservable.fromKeyEvents(textField).subscribe { showCorrections() }
     }
 
@@ -34,22 +37,17 @@ class App : JPanel(GridBagLayout()) {
     companion object {
 
         private fun createAndShowGUI() {
-            //Create and set up the window.
-            val frame = JFrame("TextDemo")
+            val frame = JFrame("Autocorrect")
             frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-
-            //Add contents to the window.
             frame.add(App())
-
-            //Display the window.
             frame.pack()
             frame.isVisible = true
         }
 
         @JvmStatic
         fun main(args: Array<String>) {
-            //Schedule a job for the event dispatch thread:
-            //creating and showing this application's GUI.
+            // Schedule a job for the event dispatch thread:
+            // creating and showing this application's GUI.
             javax.swing.SwingUtilities.invokeLater { createAndShowGUI() }
         }
     }
